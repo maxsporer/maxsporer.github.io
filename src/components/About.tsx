834d86427be2e15b072773e945ffb665febb1d3e
Max <sporer_maximilian@bah.com>
2024-04-24 16:03:17 -0400

    update About section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -37,7 +37,7 @@ function About() {
         </div>
       </div>
       <div className="pt-8">
-        I previously worked at <a className="link-primary" href="https://www.yext.com/">Yext</a> as a Software Engineer. Prior to that, I was a Data Analyst at <a className="link-primary" href="https://www.linkit.com/">LinkIt</a>. I graduated from Princeton in 2021 with a degree in Operations Research and Financial Engineering. I grew up in Morristown, NJ and my hobbies include running, kickboxing, and playing the guitar.
+        I previously worked at <a className="link-primary" href="https://www.yext.com/">Yext</a> as a Software Engineer. Prior to that, I was a Data Analyst at <a className="link-primary" href="https://www.linkit.com/">LinkIt</a>. I graduated from Princeton in 2021 with a degree in Operations Research and Financial Engineering.
       </div>
       <div className="pt-4">
         You can contact me at maxsporer (at) gmail (dot) com
